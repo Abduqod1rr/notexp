90a98a1b2514a4e1dfc93e2330bbf8da9e9be0c0
abduqodir <ttheonepersonn@gmail.com>
2025-12-22 11:11:35 +0000

    setting up stuf

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView ,DeleteView,ListView ,UpdateView
+from forms import UserForm
 # Create your views here.
+
+
+class RegisterUser(CreateView):
+    form_class=UserForm
+    template_name=''
+    success_url=''
+    
